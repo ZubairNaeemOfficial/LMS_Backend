a4931da0c6682664ce01aee65ce46da9fd45d425
zubair4342 <zubair@mapayus.com>
2024-05-16 18:06:49 +0500

    login and logout user api + refresh token and access token

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,7 +1,10 @@
 import express from "express"
-import { activateUser, userRegistration } from "../controllers/user.controller";
+import { activateUser, loginUser, logoutUser, userRegistration } from "../controllers/user.controller";
 const userRouter=express.Router();
 
 userRouter.post("/registeration" ,userRegistration)
 userRouter.post("/activate-user",activateUser)
+userRouter.post("/login", loginUser)
+userRouter.get("/logout", logoutUser)
+
 export default userRouter;
